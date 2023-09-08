3888f0029dbee5579cb60023c83f27ffaffd477b
= <=>
2023-09-08 16:49:54 +0500

    commit

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "../components/Navbar"
 import image from '../assets/whatsapp.png'
+import Footer from "../components/Footer"
 const HomeLayout = () => {
     return (
         <div>
             <Navbar />
             <Outlet />
+            <Footer />
             <div>
                 <a href="https://wa.me/971566181832" target='_blank'>
-                    <img src={image} alt="whatsapp-image" className="w-[100px] fixed left-0 bottom-0 z-10" />
+                    <img src={image} alt="whatsapp-image" className="w-[100px] fixed left-0 bottom-0 z-10 animate-bounce" />
                 </a>
             </div>
         </div>
