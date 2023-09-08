5353a4b13b43cc9f4194a07546b0b93ca8ee46d2
= <=>
2023-09-08 16:12:40 +0500

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <nav className='bg-red-400'>
       <div className='align-element py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8 justify-between'>
         <h2 className='text-3xl font-bold'>
-          SJ<span className='text-red-600'>IS</span>
+          SAJ<span className='text-red-600'>IS</span>
         </h2>
         <div className='flex gap-x-3'>
           {links.map((link) => {
