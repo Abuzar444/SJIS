d2b554e2ac0bd59c456a840fea66ab553a573423
= <=>
2023-09-10 15:38:24 +0500

    commit

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,15 +1,30 @@
-import heroImg from '../assets/hero.jpg';
+import hero1 from '../assets/hero1.jpg'
+import hero2 from '../assets/hero2.jpg'
+import hero3 from '../assets/hero3.jpg'
+import hero4 from '../assets/hero-4.jpg'
+
+const heroImages = [hero1, hero2, hero3, hero4]
 
 const Hero = () => {
   return (
-    <div
-      style={{ backgroundImage: `url(${heroImg})` }}
-      className={`bg-no-repeat bg-cover h-screen w-full`}
-    >
-      <div className='w-full h-full flex  justify-center items-center backdrop-brightness-75 bg-red-600/30'>
-        <h1 className='text-[2rem] text-white'>Start your business in Dubai</h1>
+    <div className="grid lg:grid-cols-2 align-element gap-24 items-center py-14">
+      {/* info */}
+      <div>
+        <h1 className='max-w-2xl text-4xl font-bold tracking-tight sm:text-6xl'>We are changing the way people print..</h1>
+        <p className='mt-8 max-w-xl text-lg leading-8'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, non! Obcaecati corrupti id aperiam corporis enim sapiente eligendi sit ut.</p>
+      </div>
+
+      {/* crousel */}
+      <div className="hidden h-[28rem] lg:carousel carousel-center p-4 space-x-4 bg-neutral rounded-box">
+        <div className="carouse-item flex">
+          {heroImages.map((image) => {
+            return (
+              <img src={image} className='rounded-box h-full w-80 object-cover' />
+            )
+          })}
+        </div>
       </div>
     </div>
-  );
-};
-export default Hero;
+  )
+}
+export default Hero
